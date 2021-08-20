5c330c3fdb501302436fb49250217ddae60e101a
furevor <furevor@gmail.com>
2021-08-20 08:42:11 +0300

    added some html and styles with html templates

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    return render(request, 'main/index.html')
+
+
+def about(request):
+    return render(request, 'main/about-us.html')
