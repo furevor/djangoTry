bbba8768bfcc52651f10b749be16bc84ec84763b
furevor <furevor@gmail.com>
2021-08-30 17:33:37 +0300

    added create task form functional

---
+++ main/forms.py
@@ -0,0 +1,17 @@
+from .models import Task
+from django.forms import ModelForm, TextInput, Textarea
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "task"]
+        widgets = {
+            "title": TextInput(attrs={
+                'class': 'form-control mb-2',
+                'placeholder': 'Введите название'
+            }),
+            "task": Textarea(attrs={
+                'class': 'form-control mb-2',
+                'placeholder': 'Введите описание'
+            })
+        }
